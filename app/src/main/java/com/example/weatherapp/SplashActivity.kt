b9a8b593fa5534b73f436b176ce2eb09d849f631
ayuhzkishan <ayush.kishan29@gmail.com>
2024-08-17 13:49:27 +0530

    finished project basic implementations

--- app/src/main/java/com/example/weatherapp/SplashActivity.kt
+++ app/src/main/java/com/example/weatherapp/SplashActivity.kt
@@ -21,7 +21,7 @@ class SplashActivity : AppCompatActivity() {
             val intent = Intent(this,MainActivity::class.java)
             startActivity(intent)
             finish()
-        },3000)
+        },1800)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
